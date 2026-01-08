71299d227584e4c810e88ba811acc8221a735ef0
Wes Wang <wesw@kevgroup.com>
2026-01-08 11:54:44 -0800

    setup routing for different versions of the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Link, Route, Routes } from "react-router-dom";
 import Table1 from "./table1/Table1";
 
+function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Component Demos</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/table1">Table 1</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
 function App() {
-  return <Table1 />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/table1" element={<Table1 />} />
+    </Routes>
+  );
 }
 
 export default App;
